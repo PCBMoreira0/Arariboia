33d88a5622f6b2b63bed86020c5157b37fdfca24
PCBMoreira2 <58518350+PCBMoreira2@users.noreply.github.com>
2024-02-02 13:44:58 -0300

    Changed main and Wokwi Simulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
-#include <freertos/FreeRTOS.h>
+#include <freertos\FreeRTOS.h>
 
-constexpr int motorRPMPin = GPIO_NUM_36;
+#define pin 2
 
 // Variables modified by ISR
 volatile unsigned long current_duration;
@@ -13,109 +13,108 @@ QueueHandle_t average_mailbox;
 TaskHandle_t average_task;
 SemaphoreHandle_t average_data_mutex;
 
+// Time variables
+int show_display_delay = 500; // ms
 
-float frequencyToRPM(float signalFrequency)
+float rpm_line(float t)
 {
-	constexpr float angularCoefficient = 15.179f;
-	constexpr float linearCoefficient = -19.65f;
-	return angularCoefficient * signalFrequency + linearCoefficient;
+  return 15.179 * t - 19.65;
 }
 
-IRAM_ATTR void calculateFrequencyISR() 
+void calc_Freq_ISR()
 {
-	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-	unsigned long duration = micros();
-	xSemaphoreTakeFromISR(average_data_mutex, &xHigherPriorityTaskWoken);
-	total_period += duration - current_duration;
-	current_duration = duration;
-	count_measures++;
-	xSemaphoreGiveFromISR(average_data_mutex, &xHigherPriorityTaskWoken);
+  unsigned long duration = micros();
+  xSemaphoreTakeFromISR(average_data_mutex, &xHigherPriorityTaskWoken);
+  total_period += duration - current_duration;
+  current_duration = duration;
+  count_measures++;
+  xSemaphoreGiveFromISR(average_data_mutex, &xHigherPriorityTaskWoken);
 
-	portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
-void calculateAverageFrequency(void *parameters) {
-	while (1) {
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-	int total = total_period;
-	int count = count_measures;
-	xSemaphoreTake(average_data_mutex, portMAX_DELAY);
-	total_period = 0;
-	count_measures = 0;
-	xSemaphoreGive(average_data_mutex);
+void calculate_frequency(void *parameters)
+{
+  float frequency;
 
-	float period = (float)total / count;
-	float frequency = 1 / (period / 1000000);
+  while (1)
+  {
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    int total = total_period;
+    int count = count_measures;
+    xSemaphoreTake(average_data_mutex, portMAX_DELAY);
+    total_period = 0;
+    count_measures = 0;
+    xSemaphoreGive(average_data_mutex);
 
-	xQueueSend(average_mailbox, (void *)&frequency, portMAX_DELAY);
-	}
+    float period = (float)total / count;
+    frequency = 1 / (period / 1000000);
 
-	vTaskDelete(NULL);
+    xQueueSend(average_mailbox, (void *)&frequency, portMAX_DELAY);
+  }
+
+  vTaskDelete(NULL);
 }
 
-void printValues(void *parameters) {
+void show_display(void *parameters)
+{
+  float frequency;
 
-	// Time variables
-	constexpr int show_display_delay_ms = 250;
-	float frequency;
+  while (1)
+  {
+    xTaskNotifyGive(average_task);
+    if (xQueueReceive(average_mailbox, (void *)&frequency, portMAX_DELAY))
+    {
+      Serial.println(rpm_line(frequency));
 
-	while (1) {
-	xTaskNotifyGive(average_task);
-	if (xQueueReceive(average_mailbox, (void *)&frequency, portMAX_DELAY)) {
-		float rpm = frequencyToRPM(frequency);
-		
-		/*
-		if (rpm != NAN) {
-		Serial.printf("RPM not detected\n");
-		} else {
-		Serial.printf("RPM: %f\n", rpm);
-		
-		}
-		*/
-		Serial.printf("Frequency: %.0f\t RPM: %.0f\n", frequency, rpm);
-		vTaskDelay(pdMS_TO_TICKS(show_display_delay_ms));
-	}
-	}
+      vTaskDelay(show_display_delay / portTICK_PERIOD_MS);
+    }
+  }
 
-	vTaskDelete(NULL);
+  vTaskDelete(NULL);
 }
 
+void debug_motor(void *parameters)
+{
+  int pot;
+  const float v = 0.02442;
+  while (1)
+  {
+    digitalWrite(pin, HIGH);
+    pot = analogRead(4);
+    float p = ((1 / (pot * v)) / 2) * 1000;
+    vTaskDelay(p / portTICK_PERIOD_MS);
+    digitalWrite(pin, LOW);
+    vTaskDelay(p / portTICK_PERIOD_MS);
+  }
 
-//! WOKWI DEBUG
-void debug_motor(void *parameters) {
-	int pot;
-	const float potentiometerConstant = 0.02442f;
-	while (1)
-	{
-	digitalWrite(motorRPMPin, HIGH);
-	pot = analogRead(4);
-	float p = ((1 / (pot * potentiometerConstant)) / 2) * 1000;
-	vTaskDelay(p / portTICK_PERIOD_MS);
-	digitalWrite(motorRPMPin, LOW);
-	vTaskDelay(p / portTICK_PERIOD_MS);
-	}
-
-	vTaskDelete(NULL);
+  vTaskDelete(NULL);
 }
 
-void Initialize() {
-	pinMode(motorRPMPin, INPUT);
-	average_mailbox = xQueueCreate(1, sizeof(float));
-	average_data_mutex = xSemaphoreCreateMutex();
-	attachInterrupt(motorRPMPin, calculateFrequencyISR, RISING);
-	xTaskCreate(calculateAverageFrequency, "Calculate-Average", 4096, NULL, 1, &average_task);
-	xTaskCreate(printValues, "Show-Display", 4096, NULL, 1, NULL);
-	//xTaskCreate(debug_motor, "Debug-Switching", 2000, NULL, 2, NULL);
+void Initialize()
+{
+  pinMode(pin, OUTPUT);
+  average_mailbox = xQueueCreate(1, sizeof(float));
+  average_data_mutex = xSemaphoreCreateMutex();
+  attachInterrupt(pin, calc_Freq_ISR, RISING);
+  xTaskCreate(calculate_frequency, "Calculate-Average", 2000, NULL, 1, &average_task);
+  xTaskCreate(show_display, "Show-Display", 1000, NULL, 1, NULL);
+  xTaskCreate(debug_motor, "Debug-Switching", 2000, NULL, 2, NULL);
 }
 
-void setup() {
-	Serial.begin(115200);
-	vTaskDelay(pdMS_TO_TICKS(1000));
-	Serial.printf("\n---INICIALIZANDO---\n");
-	Initialize();
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("--- INICIALIZANDO ---");
+
+  Initialize();
+
+  Serial.println("--- TÉRMINO ---");
 }
 
-void loop() {
-	vTaskDelete(NULL);
+void loop()
+{
 }
